cd662c271d3e2fab5b6b51e460849de23a3db2a7
Tareq Otaibi <tareqdev@protonmail.com>
2020-05-04 10:17:11 +0300

    cleaning code

--- src/Components/RequestPosts.js
+++ src/Components/RequestPosts.js
@@ -1,15 +1,13 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { getAllPost } from "../Context/APICalls";
 import { postContext } from "../Context/PostContext";
 
 const RequestPost = () => {
   const [post, setPost] = useContext(postContext);
-  const [buttonState, setButton] = useState();
 
   const requestHandler = async () => {
-    const resutls = await getAllPost();
-    setButton("disable");
-    setPost(resutls);
+    const request = await getAllPost();
+    setPost(request);
   };
 
   return <button onClick={requestHandler}>Get Request</button>;
